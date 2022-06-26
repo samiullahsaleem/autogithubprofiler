4383e661748ce943efddef343d47259440d88f6f
Sami Ullah Saleem <54318487+samiullahsaleem@users.noreply.github.com>
2022-06-26 22:37:47 +0000

    version 1.1

--- src/App.js
+++ src/App.js
@@ -1,29 +1,71 @@
 import { useState } from 'react';
-import {TextField,Button} from '@mui/material'
-
+import { TextField, Button } from '@mui/material'
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import { styled } from '@mui/material/styles';
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 function App() {
-  const [cgraph, setgraph] = useState('samiullahsaleem');
-  const graphC = ()=>{
-    let val = document.getElementById("cgraphname").value;
-    console.log(val);
-    return setgraph(val);
+  const [username, setUsername] = useState('samiullahsaleem');
+  const graphC = () => {
+    let val = document.getElementById("usernamename").value;
+    return setUsername(val);
   }
   return (
     <div >
-      <h1 style={{ textAlign: "center" }}>Welcome to Auto Awesome Github Profile Generator</h1>
-      <TextField id="cgraphname" name="cgraphname" label="Github Username" variant="standard" defaultValue={"samiullahsaleem"}/>
-      <br/>
-      <Button variant="contained" onClick={graphC}>Click Here</Button>
-      <br/>
-      <h3>Contribution Graph</h3>
-      <img height="180em" alt="Browser doesn't support" src={`https://activity-graph.herokuapp.com/graph?username=${cgraph}&theme=xcode`} />
-      <br/>
-      <h3>⚙️ GitHub Analytics</h3>
-      <img height="180em" width="25%" alt="Browser doesn't support or Username not found" src={`https://github-readme-stats.vercel.app/api?username=${cgraph}&show_icons=true&theme=algolia&include_all_commits=true&count_private=true `} />
-      <img height="180em" width="15%" alt="Browser doesn't support or Username not found" src={`https://github-readme-stats-eight-theta.vercel.app/api/top-langs/?username=${cgraph}&layout=compact&langs_count=8&theme=algolia`}/>
+      <h1 style={{ textAlign: "center" }}>👋 Welcome to Auto Awesome Github Profile Generator</h1>
+      <TextField id="usernamename" name="usernamename" label="Github Username" variant="standard" defaultValue={"samiullahsaleem"} />
       <br />
-      <img width="50%" alt="Browser doesn't support or Username not found" src={`https://github-readme-streak-stats.herokuapp.com/?user=${cgraph}&show_icons=true&locale=en&layout=demo&theme=merko&hide_border=true`} />
-    </div>
+      <Button variant="contained" onClick={graphC}>Click Here</Button>
+      <br />
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={2}>
+          <Grid item xs={6}>
+            <Item>
+              <h3>🫵 Github Profile View</h3>
+              <h3>Contribution Graph</h3>
+              <img height="180em" width="100%" alt="Browser doesn't support" src={`https://activity-graph.herokuapp.com/graph?username=${username}&theme=xcode`} />
+              <br />
+              <h3>⚙️ GitHub Analytics</h3>
+              <img height="180em" width="100%"  alt="Browser doesn't support or Username not found" src={`https://github-readme-stats.vercel.app/api?username=${username}&show_icons=true&theme=algolia&include_all_commits=true&count_private=true `} />
+              <br />
+              <img height="180em" width="100%"  alt="Browser doesn't support or Username not found" src={`https://github-readme-stats-eight-theta.vercel.app/api/top-langs/?username=${username}&layout=compact&langs_count=8&theme=algolia`} />
+              <br />
+              <img width="50%" alt="Browser doesn't support or Username not found" src={`https://github-readme-streak-stats.herokuapp.com/?user=${username}&show_icons=true&locale=en&layout=demo&theme=merko&hide_border=true`} />
+            </Item>
+          </Grid>
+          <Grid item xs={6}>
+            <Item>
+              <h3>💻Code</h3>
+              <div>
+                {`## Contribution Graph`}
+                <br />
+                {`[![${username}'s GitHub Activity Graph](https://activity-graph.herokuapp.com/graph?username=${username}&theme=rogue)](https://github.com/${username})`}
+                <br />
+                {`<h2><i>⚙️ GitHub Analytics</i></h2>`}
+                <br />
+                {`<p align="center">`}
+                <br />
+                {`<img height="180em" src="https://github-readme-stats.vercel.app/api?username=${username}&show_icons=true&theme=algolia&include_all_commits=true&count_private=true"/>`}
+                <br />
+                {`<img height="180em" src="https://github-readme-stats-eight-theta.vercel.app/api/top-langs/?username=${username}&layout=compact&langs_count=8&theme=algolia" />`}
+                <br />
+                {`<img width="90%" src="https://github-readme-streak-stats.herokuapp.com/?user=${username}&show_icons=true&locale=en&layout=demo&theme=merko&hide_border=true" />`}
+              </div>
+            </Item>
+          </Grid>
+        </Grid >
+      </Box >
+      <footer>
+        <h2 style={{ textAlign: "center", backgroundColor: "black", color: "white" }}>Made with 💖 by Sami Ullah Saleem</h2>
+      </footer>
+    </div >
   );
 }
 
